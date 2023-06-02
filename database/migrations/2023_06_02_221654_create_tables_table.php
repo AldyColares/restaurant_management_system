dc03423f465b06a784806cd233ab78463a3688d3
aldycolares <aldycolares@gmail.com>
2023-06-02 19:32:18 -0300

    add migration admin user and Tables

---
+++ database/migrations/2023_06_02_221654_create_tables_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tables', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('code_name');
+            $table->timestamps();
+        });
+
+        Schema::create('reservation_tables', function (Blueprint $table){
+            $table->increments('id');
+            $table->foreign('user_id')->references('id')->on('tables');
+            $table->date('register_table_begin');
+            $table->date('register_table_end');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tables');
+
+        Schema::dropIfExists('reservation_tables');
+    }
+};
